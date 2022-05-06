65c86c3a719c42dd8542fcc11200a85fc96aa837
李荣潭 <844928336@qq.com>
2022-05-06 15:18:10 +0800

    Initial commit

---
+++ Example/LRTBaseComponent/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LRTBaseComponent
+//
+//  Created by 李荣潭 on 05/06/2022.
+//  Copyright (c) 2022 李荣潭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
